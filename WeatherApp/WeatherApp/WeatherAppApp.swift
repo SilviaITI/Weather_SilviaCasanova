5079dfa86299d79a3176060322275f78ae106e6c
silviaITI <silvia.casanovamartinez@gmail.com>
2024-10-17 16:43:34 +0200

    first commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Silvia Casanova Martinez on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
